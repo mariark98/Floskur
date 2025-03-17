7b806c6334b80ef330ed221ee1630e80d63ab8e6
mariark98 <mrk7@hi.is>
2025-03-17 20:53:08 +0000

    ónotað import strokað út

--- src/main/java/org/example/floskur/FloskurController.java
+++ src/main/java/org/example/floskur/FloskurController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
 
 import java.net.URL;
 import java.util.ResourceBundle;
